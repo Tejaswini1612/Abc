31343f5db332d7c1335f330c38cfc728be90bcb8
Sagar <Sagar@SAGAR>
2020-10-13 15:58:30 +0530

    by tej

--- GitProject/src/com/cjc/Demo.java
+++ GitProject/src/com/cjc/Demo.java
@@ -4,6 +4,11 @@ package com.cjc;
 
 public class Demo {
 	
+	public void sum()
+	{
+		System.out.println("sum");
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println("Hello...Main Method");
